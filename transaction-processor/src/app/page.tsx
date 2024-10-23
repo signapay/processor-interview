59e856a05ff04763cfeadbf8e56a24d616b2d85b
Jacob Sexton <jsextonprofessional@protonmail.com>
2024-10-23 17:57:02 -0500

    Removes todos. Fixes imports.

--- transaction-processor/src/app/page.tsx
+++ transaction-processor/src/app/page.tsx
@@ -1,21 +1,8 @@
 'use client'
 
-// TO DO
-// write readme
-
-// FINAL QA
-// remove comments and console logs
-
-// NICE TO HAVE
-// paginate table OR at least lazy load data
-// fix overlap of collapsed table cells
-// favicon
-// highlight button of selected section
-// conditionally render button text based on if file has been uploaded or not
-
 import { TransactionProvider } from './context/context';
-import { Content } from './components/Content';
 import { WidthContainer } from './components/WidthContainer';
+import { Content } from './components/Content';
 import { Navbar } from './features/navbar';
 
 export default function Home() {
