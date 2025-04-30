9228b8f0d85cd04de2b67dee1f28fd1465e3d4af
Andrea S <maydrea@gmail.com>
2025-04-30 16:45:11 -0500

    Added filter to the transaction index

--- SignaPayProcessor/Pages/Transaction/Index.cshtml.cs
+++ SignaPayProcessor/Pages/Transaction/Index.cshtml.cs
@@ -20,10 +20,36 @@ namespace SignaPayProcessor.Pages_Transaction
         }
 
         public IList<Transaction> Transaction { get;set; } = default!;
+        
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? CardType { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public DateTime? TransactionDate { get; set; }
 
         public async Task OnGetAsync()
         {
             Transaction = await _context.Transaction.ToListAsync();
+             if(!string.IsNullOrEmpty(SearchString))
+            {
+                Transaction = Transaction
+                    .Where(s => s.CardNumber.ToString().Contains(SearchString)).ToList();
+            }
+            if(!string.IsNullOrEmpty(CardType))
+            {
+                Transaction = Transaction
+                    .Where(s => s.CardType != null && CardType != null && s.CardType.ToString()
+                    .Contains(CardType))
+                    .ToList();
+            }
+            if(TransactionDate != null)
+            {
+                Transaction = Transaction
+                    .Where(s => s.TimeStamp.Date == TransactionDate?.Date).ToList();
+            }
         }
     }
 }
